9ae43563eb0f134fdb13d59bbc50c18bc0a4fcaf
InveterateCoder <inveterate.coder@gmail.com>
2020-05-23 12:06:53 +0300

    my own service

---
+++ ClientApp/public/rooms-service-worker.js
@@ -0,0 +1,29 @@
+const CURRENT_CACHE = "rooms2.0";
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cachesKeys => {
+            return Promise.all(
+                cachesKeys.map(cacheKey => {
+                    if (cacheKey !== CURRENT_CACHE)
+                        return caches.delete(cacheKey);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    if (event.request.url.startsWith('http') && event.request.method === 'GET'
+        && !event.request.url.startsWith(new URL('api', location.origin)))
+    {
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request).then(response => {
+                return caches.open(CURRENT_CACHE).then(cache => {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        });
+    }
+});
