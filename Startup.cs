aa6b610e659d6b2d3846857b2f86e0a0f08a39e8
InveterateCoder <inveterate.coder@gmail.com>
2020-10-15 17:05:12 +0300

    update

--- Startup.cs
+++ Startup.cs
@@ -3,7 +3,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
